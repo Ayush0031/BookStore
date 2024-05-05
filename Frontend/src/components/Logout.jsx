1dd9466939f078c0cef9dcc955e6627434f0d8a4
AYUSH <89582411+Ayush0031@users.noreply.github.com>
2024-05-05 20:01:40 +0530

    Login Authentication

---
+++ Frontend/src/components/Logout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logout() {
+  return (
+    <div>
+      <button className='px-3 py-2 bg-red-500 text-white rounded-md cursor-pointer'>Logout</button>
+    </div>
+  )
+}
+
+export default Logout
